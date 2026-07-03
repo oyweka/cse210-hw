a1e01d8385c605c610d46d17aa1e8f46eb8d777e
oyweka <alexoyweka@gmail.com>
2026-07-03 16:32:05 +0300

    Done exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        string letter = "";
+        string sign = "";
+
+        if (gradePercentage < 0 || gradePercentage > 100)
+        {
+            Console.WriteLine("The value must be between 0 and 100.");
+        }
+        else
+        {
+            if (gradePercentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+            if (gradePercentage % 10 >= 7 && letter != "A" && letter != "F")
+            {
+                sign = "+";
+            }
+            else if (gradePercentage % 10 < 3 && letter != "F" && gradePercentage != 100)
+            {
+                sign = "-";
+            }
+            Console.WriteLine($"Your grade is: {letter}{sign}");
+
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed.");
+            }
+            else
+            {
+                Console.WriteLine("Wish you all the best next time.");
+            }
+        }
     }
 }
