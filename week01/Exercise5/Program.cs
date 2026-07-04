55b7b500483b1071a6e94c989fc4baa8db2d1c7b
oyweka <alexoyweka@gmail.com>
2026-07-04 14:30:01 +0300

    Comple Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        Console.WriteLine($"Hi {userName}, the square of your number is: {squaredNumber}");
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int userNumber)
+    {
+        return userNumber * userNumber;
     }
 }
