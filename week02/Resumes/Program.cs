8edb6568676c34c068ba766da4d299a1e82652c8
oyweka <alexoyweka@gmail.com>
2026-07-09 21:55:01 +0300

    Resume complete

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Safaricom";
+        job1._jobTitle = "Machine Learning Engineer";
+        job1._startYear = "2019";
+        job1._endYear = "2020";
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = "2020";
+        job2._endYear = "2023";
+
+        Resume myResume = new Resume();
+        myResume._name = "Alex Oyweka";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
