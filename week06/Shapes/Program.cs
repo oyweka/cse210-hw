31762c00bc3379609b35196fbcbdcba026dc694c
oyweka <alexoyweka@gmail.com>
2026-08-08 16:42:23 +0300

    Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle("Red", 7));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Square("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
